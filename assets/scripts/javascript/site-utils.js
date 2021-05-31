1ca947aa1725726c87d4e959e7aefd7a60d62e03
Eliseontwerpt <harmjan@eliseontwerpt.nl>
2021-05-31 16:57:09 +0200

    fix height

--- assets/scripts/javascript/site-utils.js
+++ assets/scripts/javascript/site-utils.js
@@ -7,7 +7,7 @@ function scrollFunction() {
     document.getElementById("nav-header").style.display = "none";
     
   } else if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    document.getElementById("nav-brand").height = 80 - document.body.scrollTop;
+    document.getElementById("nav-brand").height = 100 - document.body.scrollTop;
     document.getElementById("nav-header").style.display = "none";
       }
   
@@ -15,7 +15,7 @@ function scrollFunction() {
   else {
 
     document.getElementById("navbar").style.height = "";
-    document.getElementById("nav-brand").height = 80;
+    document.getElementById("nav-brand").height = 100;
      document.getElementById("nav-header").style.display = "";
   }
 }
